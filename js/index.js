25dea0873ce5f67c4a12b682fbaef5ae630e27df
Roman Vinichenko <chicagezzz@gmail.com>
2021-08-06 16:17:53 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 50) {
-    $('.header').addClass('header--active');
-  } else {
-    $('.header').removeClass('header--active');
-  }
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('.header').addClass('header--active');
+    } else {
+      $('.header').removeClass('header--active');
+    }
+  });
+  $('.top__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+  });
+  $('.menu a, .footer a').on('click', function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate(
+      {
+        scrollTop: top,
+      },
+      1200,
+    );
+  });
 });
